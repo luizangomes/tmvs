f7a1d0ef25d685024c0fa1d9eb43179ecbf36a52
dw <dw@dwdesktop.(none)>
2013-02-26 15:34:31 +0100

    Added GPL

--- src/IOArrayHelpers.hs
+++ src/IOArrayHelpers.hs
@@ -1,3 +1,21 @@
+--Copyright 2010 David White
+
+--This file is part of Tiny Machine Visual Simulator.
+
+--Tiny Machine Visual Simulator is free software: you can redistribute 
+--it and/or modify it under the terms of the GNU General Public License 
+--as published by the Free Software Foundation, either version 3 of the 
+--License, or (at your option) any later version.
+
+--Tiny Machine Visual Simulator is distributed in the hope that it 
+--will be useful, but WITHOUT ANY WARRANTY; without even the implied 
+--warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+--See the GNU General Public License for more details.
+
+--You should have received a copy of the GNU General Public License
+--along with Tiny Machine Visual Simulator.  If not, see 
+--<http://www.gnu.org/licenses/>.
+
 module IOArrayHelpers where
 
 import Graphics.UI.Gtk
